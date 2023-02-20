fd24f91fbb300fd807b85b6b58c5f8c61ecdad0b
Aditi Acharya <alishaacharya77@gmail.com>
2023-02-20 21:19:11 +0545

    created route to navigate to external route page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "contact", to: "pages#contact"
   get "about", to: "pages#about"
+  get "blog", to: redirect("https://aditi.com.np")
 
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
