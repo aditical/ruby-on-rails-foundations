8932578417637365ab920c0ddc65316f47375235
Aditi Acharya <alishaacharya77@gmail.com>
2023-02-20 21:21:39 +0545

    created routes for error page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "contact", to: "pages#contact"
   get "about", to: "pages#about"
   get "blog", to: redirect("https://aditi.com.np")
+  get "error", to: "pages#error"
 
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "pages#home"
+  get "*path", to: redirect("/error")
 end
