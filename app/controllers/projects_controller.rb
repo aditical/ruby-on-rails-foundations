6cc3c65e316546f68233def56b58e603bbc8f61f
Aditi Acharya <alishaacharya77@gmail.com>
2023-02-22 12:20:49 +0545

    customized message on the controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -25,7 +25,7 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       if @project.save
-        format.html { redirect_to project_url(@project), notice: "Project was successfully created." }
+        format.html { redirect_to project_url(@project), notice: "Congratulations!! A new project is created." }
         format.json { render :show, status: :created, location: @project }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class ProjectsController < ApplicationController
     @project.destroy
 
     respond_to do |format|
-      format.html { redirect_to projects_url, notice: "Project was successfully destroyed." }
+      format.html { redirect_to projects_url, notice: "The project is deleted sucessfully." }
       format.json { head :no_content }
     end
   end
