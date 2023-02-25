5712ba1c6c37adb4eb7ecc35e4c4c0d030588a01
Aditi Acharya <alishaacharya77@gmail.com>
2023-02-25 21:02:29 +0545

    created nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
+
   get "contact", to: "pages#contact"
   get "about", to: "pages#about"
   get "blog", to: redirect("https://aditi.com.np")
   get "error", to: "pages#error"
 
-  resources :projects
+  resources :projects do
+    resources :tasks, except: [:index], controller: "projects/tasks"
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
