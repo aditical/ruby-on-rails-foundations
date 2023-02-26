6230ac3fcbe2d6cda3c9473e40928ae3184a4276
Aditi Acharya <alishaacharya77@gmail.com>
2023-02-26 09:30:34 +0545

    created a devise user model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_24_153900) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_26_034301) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -29,6 +29,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_153900) do
     t.boolean "completed"
     t.text "task_file"
     t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "tasks", "projects"
